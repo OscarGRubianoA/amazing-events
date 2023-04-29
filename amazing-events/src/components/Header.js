0447716b1aef90a8b3ed466dd35f9f82801c86ee
BetoCochas <oscargio86@hotmail.com>
2023-04-28 23:01:43 -0500

    'component'

---
+++ amazing-events/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header (){
+    return(
+        <div>
+        <nav>
+    <a>Home</a>
+    <a>Contact</a>
+    <a>About</a>
+
+    </nav>
+    
+        </div>
+    )
+}
+export default Header;
