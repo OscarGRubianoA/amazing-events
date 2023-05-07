83be455896750272f658d5bc7474a0eebaeba303
BetoCochas <oscargio86@hotmail.com>
2023-05-06 21:04:07 -0500

    'eventspagejs'

---
+++ amazing-events/src/pages/EventsPage.js
@@ -0,0 +1,10 @@
+import WebSiteLayout from "../layouts/WebSiteLayout"
+function EventsPage(){
+    return(
+        <WebSiteLayout>
+            <h1>Events</h1>
+
+        </WebSiteLayout>
+    )
+}
+export default EventsPage
