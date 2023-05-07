0ee3f9e344a429bc400fad3a98dfdbc528289c21
BetoCochas <oscargio86@hotmail.com>
2023-05-07 18:34:42 -0500

    'appjs'

--- amazing-events/src/App.js
+++ amazing-events/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
+import Carousel from './components/Carousel';
 
 function App() {
   return(
     <div>
 {/* <Header/> */}
 {/* <WelcomePage/> */}
-<HomePage/>
+<Carousel/>
 {/* <EventsPage/> */}
 </div>
 )
