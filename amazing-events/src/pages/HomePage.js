7bad0e669e006dd1638ed52d1ec85285e6f009e6
BetoCochas <oscargio86@hotmail.com>
2023-05-06 21:04:51 -0500

    homepagejs'

---
+++ amazing-events/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+
+import WebSiteLayout from "../layouts/WebSiteLayout"
+
+function HomePage(){
+    return(
+        <WebSiteLayout>
+        <h1>Start</h1>
+
+    </WebSiteLayout>
+    )
+}
+export default HomePage
