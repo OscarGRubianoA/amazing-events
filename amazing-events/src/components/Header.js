aa4452549cda6f6b0740943e33536069b7acb417
BetoCochas <oscargio86@hotmail.com>
2023-05-06 21:02:49 -0500

    'headerjs'

--- amazing-events/src/components/Header.js
+++ amazing-events/src/components/Header.js
@@ -1,10 +1,10 @@
 function Header (){
     return(
         <div>
-            Esta es la barra de navegacion.
-   <nav><a href="">Home</a>
-   <a href="">About</a>
-   <a href="">Contact</a>
+            
+   <nav><a href="#">Home</a>
+   <a href="#">About</a>
+   <a href="#">Contact</a>
    </nav>
         </div>
     )
