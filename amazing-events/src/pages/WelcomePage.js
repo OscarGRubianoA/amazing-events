f66174fe1d3af53839b13697503c1d23b9f44326
BetoCochas <oscargio86@hotmail.com>
2023-09-14 20:49:13 -0500

    'welcome'

--- amazing-events/src/pages/WelcomePage.js
+++ amazing-events/src/pages/WelcomePage.js
@@ -13,7 +13,7 @@ function WelcomePage(){
               target="_blank"
               rel="noopener noreferrer"
             >
-              ¡¡¡ Aprende Reactjs facilmente ahora !!!
+              ¡¡¡ Aprende React facilmente ahora !!!
             </a>
           </header>
         </div>
